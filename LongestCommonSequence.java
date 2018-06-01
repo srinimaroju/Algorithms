8bbcdbea0c681bf1505cec5260b2f020b72e2b34
Srinivas Maroju <srinivas.maroju@nbcuni.com>
2018-06-01 10:59:46 -0400

    Updates

--- LongestCommonSequence.java
+++ LongestCommonSequence.java
@@ -12,40 +12,46 @@ import java.util.HashMap;
 
 public class LongestCommonSequence {
 	public static void main(String[] args) {
-		LongestCommonSequence.printPretty("abcabcbb");
+		LongestCommonSequence.printPretty("abcabcdbb");
 		LongestCommonSequence.printPretty("bbbb");
 		LongestCommonSequence.printPretty("pwwkew");
+		LongestCommonSequence.printPretty("dvdf");
 		LongestCommonSequence.printPretty("adbcabacdeffabcz");
+		//LongestCommonSequence.printPretty("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefg..");
 	}
 	public String calculateLcs(String input) {
+		//System.out.println(input);
 		HashMap <Character, Integer> bufferMap = new HashMap<>();
 		int current_lcs = 0; //Stores current LCS 
 		int max_lcs = 0;
-		int index_start_max_lcs_sb = 0;
-		int index_end_max_lcs_sb = 0;
-		int index_start_current_lcs_sb = 0;
-		
+		int index_start_max_lcs = 0;
+		int index_end_max_lcs = 0;
+		int index_start_current_lcs = 0;
+		int index_end_current_lcs = 0;
+		if(input.length()==0) {
+			return "";
+		}
 		for(int i=0,length=input.length(); i<length; i++){
 			Character c = (Character)input.charAt(i);
-			if(bufferMap.containsKey(c)) {
-				//Repeat..Restart to check for new sequence
-				current_lcs = 1;
-				index_start_current_lcs_sb = i;
-				bufferMap.clear();
-				bufferMap.put(c,1);
+
+			if(bufferMap.containsKey(c) && ( bufferMap.get(c) >= index_start_current_lcs )) {
+			    int cur_index = bufferMap.get(c);
+				current_lcs =  i-cur_index;
+				index_start_current_lcs =  cur_index + 1;
+				index_end_current_lcs = index_start_current_lcs;
 			} else {
-				bufferMap.put(c,1);
 				current_lcs++;
-				if (current_lcs > max_lcs) {
-					 max_lcs = current_lcs;
-					 index_start_max_lcs_sb = index_start_current_lcs_sb;
-					 index_end_max_lcs_sb = i;
-				}
-					
 			}
+			index_end_current_lcs = i;
+			bufferMap.put(c,i);
+			if (current_lcs > max_lcs) {
+				 max_lcs = current_lcs;
+				 index_start_max_lcs = index_start_current_lcs;
+				 index_end_max_lcs = index_end_current_lcs;
+			}	
 		}
 		
-		return input.substring(index_start_max_lcs_sb, index_end_max_lcs_sb+1);
+		return input.substring(index_start_max_lcs, index_end_max_lcs+1);
 	}
 	/*
 	 * Helper function to print results
@@ -55,5 +61,12 @@ public class LongestCommonSequence {
 		String result = lcs.calculateLcs(input);
 		System.out.println("Longest common substring for input --" 
 							+ input + "-- is result -- " + result + "--");
+	}
+	public void printMap(HashMap<Character, Integer> input) {
+		for (Object name: input.keySet()) {
+            String key = name.toString();
+            String value = input.get(name).toString();  
+            System.out.println(key + " " + value);  
+		} 
 	}
 }

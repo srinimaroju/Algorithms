32f635dea01b39d1e35eb7ae99cc2b113ac996ef
Srinivas Maroju <srinivas.maroju@nbcuni.com>
2019-01-17 09:26:09 -0500

    BOSTOps

---
+++ Tree/BSTOps.java
@@ -0,0 +1,174 @@
+package Tree;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class BSTOps {
+	public static void main(String []args) {
+		int[] nodes = {1,2,4,7,34,21,6,43,45,67};
+		BST tree = new BST();
+		for(int i : nodes) {
+			tree.addNode(i);
+		}
+		//tree.printUtil();
+		System.out.println("-------Tree is ---------------");
+		System.out.println(tree);
+		tree.inOrder();
+	}
+}
+
+class BST {
+	BNode root;
+	public BST() {
+		this.root = null;
+	}
+	public void printUtil() {
+		System.out.println(this.root);
+	}
+	public void addNode(int value) {
+		if(this.root == null) {
+			this.root = new BNode(value);
+		} else {
+			System.out.println("inserting node " + value);
+			this.insertNode_Iterative(value, this.root);
+		}
+	}
+	public void inOrder() {
+		System.out.println("-----In Order Recursive Travel------");
+		System.out.println(this.inOrderTraversalBFS_Recursive(this.root));
+		System.out.println("-----In Order Iterative Travel------");
+		System.out.println(this.inOrderTraversalBFS_Iterative(this.root));
+	}
+	public BNode insertNode_Iterative(int value, BNode node){
+		boolean notInserted = true;
+		while(notInserted) {
+			if(node == null) {
+				node = new BNode(value);
+				notInserted = false;
+			} else if(node.value == value) {
+				notInserted = false;
+				
+			} else if(value<node.value) {
+				if(node.left!=null) {
+					node = node.left;
+				} else {
+					node.left = new BNode(value);
+					notInserted = false;
+				}
+			} else {
+				if(node.right!=null) {
+					node = node.right;
+				} else {
+					node.right = new BNode(value);
+					notInserted = false;
+				}
+			}
+		}
+		return node;
+	}
+
+	public BNode insertNode_Recursive(int value, BNode node) {
+		if(node==null) {
+			node = new BNode(value);
+		}
+		else if(value == node.value) {
+		}
+		else if(value < node.value) {
+			//System.out.println("Insert node to left");
+			node.left = insertNode_Recursive(value, node.left);
+		} else {
+			//System.out.println("Insert node to right");
+			node.right = insertNode_Recursive(value, node.right);
+		}
+		return node;
+	}
+
+	public void removeNode(int value) {
+
+	}
+	//left, root, right
+	public String inOrderTraversalBFS_Iterative(BNode node) {
+		String result = "";
+		Stack<BNode> nodes = new Stack<BNode>();
+		BNode currentNode = node;
+		while(currentNode!=null) {
+			while(currentNode.left!=null) {
+				nodes.push(currentNode);
+				currentNode = currentNode.left;
+			}
+			result += currentNode.value + ",";
+			if(!nodes.isEmpty()) {
+				currentNode = nodes.pop();
+				result += currentNode.value + ",";
+			}
+			currentNode = currentNode.right;
+		}
+		return result;
+	}
+	
+	//Root, Left, Right
+	public String preOrderTraversalBFS_Iterative() {
+		return null;
+	}
+	//Left, Right, Root
+	public String postOrderTraversalBFS_Iterative() {
+		return null;
+	}
+	//left, root, right
+	public String inOrderTraversalBFS_Recursive(BNode node) {
+		String result = "";
+		if(node==null) return result;
+		result += this.inOrderTraversalBFS_Recursive(node.left);
+		result += node.value + ",";
+		result +=  this.inOrderTraversalBFS_Recursive(node.right);
+		return result;
+	}
+	//Root, Left, Right
+	public String preOrderTraversalBFS_Recursive() {
+		return null;
+	}
+	//Left, Right, Root
+	public String postOrderTraversalBFS_Recursive() {
+		return null;
+	}
+	public String toString() {
+		return this.toString(this.root, 1, 0);
+	}
+	private String toString(BNode node, int level, int type) {
+		String result = "";
+		if(node==null) return result;
+
+		result += this.getPadding(level);
+		
+		if(type==1) result+= "l:";
+		if(type==2) result+= "r:";
+		result +=  node.value;
+		result += "\n" + this.toString(node.left, level+1, 1) + 
+					this.toString(node.right, level+1, 2);
+
+		//this.toString()
+		return result;
+	}
+	private String getPadding(int level) {
+		StringBuffer padding = new StringBuffer();
+		for(int i=0; i<level; i++) {
+			padding.append(" ");
+		}
+		return padding.toString();
+	}
+}
+
+class BNode {
+	BNode left;
+	BNode right;
+	public int value;
+	public BNode(int value) {
+		left = right = null;
+		this.value = value;
+	}
+	public String toString() {
+		String result = this.value + " left:" + this.left + " | right:" + this.right;
+		return result;
+	}
+}
